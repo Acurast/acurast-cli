f78c8f2cf93368d9e4aa6c29fc74e70f31671697
Andreas Gassmann <andreas@andreasgassmann.ch>
2024-08-28 14:21:07 -0300

    feat: use path constants

--- src/util/readFilesInDeployFolder.ts
+++ src/util/readFilesInDeployFolder.ts
@@ -1,8 +1,9 @@
 import { readdir } from 'fs/promises'
+import { ACURAST_DEPLOYMENTS_PATH } from '../constants.js'
 
 export const readFilesInDeployFolder = async (searchString: string) => {
   // Read all files from a folder, check if string is in any of the filenames
-  const folderPath = '.acurast/deploy/'
+  const folderPath = ACURAST_DEPLOYMENTS_PATH
 
   const files = await readdir(folderPath)
 
