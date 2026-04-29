7ac0eea985fc29453fad848c1510963df9b5d27b
Andreas Gassmann <andreas@andreasgassmann.ch>
2026-04-29 11:54:47 +0200

    fix: example

--- examples/shell/acurast.sh
+++ examples/shell/acurast.sh
@@ -2,11 +2,8 @@
 set -eu
 
 echo "hello from cargo runtime"
-echo "deployment id: ${DEPLOYMENT_ID:-unset}"
-echo "bridge socket: ${BRIDGE_SOCKET:-unset}"
-uname -a
 
 curl -sS -X POST \
   -H "Content-Type: application/json" \
-  -d "{\"deploymentId\":\"${DEPLOYMENT_ID:-unset}\"}" \
+  -d "{\"message\":\"Hi from shell!\"}" \
   https://webhook.watch/api/res/ca65f4f1-da64-4c5e-8a95-0122173ccd2c
